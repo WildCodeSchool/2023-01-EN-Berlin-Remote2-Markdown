ddb61e0114ba4694b961e7bb9401709313e3258f
Deni Gogov <122295314+denigogov@users.noreply.github.com>
2023-01-18 17:29:53 +0100

    add WebSite

---
+++ beach bar/app.js
@@ -0,0 +1,16 @@
+// 'use strict';
+
+// const geoAlert = document.querySelector('.locationAlert');
+// const overlay = document.querySelector('.overlay');
+// const getLocation = document.querySelector('.get-location');
+
+// navigator.geolocation.getCurrentPosition(
+//   function (position) {
+//     console.log(position);
+//   },
+//   function () {
+//     geoAlert.style.display = 'block';
+//     overlay.style.display = 'block';
+//     getLocation.addEventListener('click', function (e) {});
+//   }
+// );
